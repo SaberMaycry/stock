b5c7f304ee3eefb50bb7c3e7c0127b9f77fa7b2d
Saber <752156785@qq.com>
2019-05-29 23:07:54 +0800

    筹码分布绘制

--- plot_stock.py
+++ plot_stock.py
@@ -1,4 +1,24 @@
 from pylab import mpl
+import pandas as pd  # This is the standard
+import matplotlib.pyplot as plt
 
 mpl.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体：解决plot不能显示中文问题
 mpl.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
+
+file_name = '05-28'
+
+df_jetton = pd.read_csv('sqlalchemy\\data\\jetton\\' + file_name + '.csv', header=1)
+
+df_jetton.columns = ['id', 'date', 'name', 'code', 'jeton', 'price', 'percentage']
+print(df_jetton)
+
+# 类型转换
+df_jetton.price = df_jetton.price.astype(float)
+df_jetton.jeton = df_jetton.jeton.astype(float)
+
+df_jetton.plot(x='price', y=['jeton'])
+plt.xlabel('股价')
+plt.ylabel('筹码')
+plt.title("广和通 {0} 筹码分布".format(file_name))
+
+plt.show()
